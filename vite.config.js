1d70e7881ec53f7f95b281b2d611d8f4fb3c1521
你的用户名 <zsx13324749258@163.com>
2025-11-03 12:07:16 +0800

    修改生产环境根目录为/,部署在nginx直接按http://localhost:8080/访问

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/lucky-draw-vue3/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
